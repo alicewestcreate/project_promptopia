552f0a790e0aab799e7d9d8ed58ea338d8258f30
Alice West <alicewest.create@gmail.com>
2023-11-02 06:58:27 +0000

    wip

---
+++ app/profile/page.jsx
@@ -0,0 +1,45 @@
+'use client'
+import {useState, useEffect} from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+
+import Profile from '@components/profile'
+
+const MyProfile = () => {
+
+    const { data: session } = useSession()
+    const [ myPosts, setMyPosts ] = useState([])
+
+    console.log("Session",session?.user.id)
+
+    useEffect(()=> {
+        const fetchPosts = async () => {
+          const response = await fetch(`api/users/${session?.user.id}/posts`)
+          const data = await response.json()
+          setMyPosts(data)
+        }
+
+        if (session?.user.id) fetchPosts()
+      },[session?.user.id])
+
+    const handleEdit = () => {
+
+    }
+    const handleDelete = async () => {
+
+    }
+
+
+  return (
+    <Profile 
+    name="My"
+    desc="Welcome to your profile page"
+    data={myPosts}
+    handleEdit={handleEdit}
+    handleDelete={handleDelete}
+    
+    />
+  )
+}
+
+export default MyProfile
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+"use client";
+import { useState, useEffect } from "react";
+
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard key={post.id} post={post} handleTagClick={handleTagClick} />
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const [searchText, setSearchText] = useState("");
+  const [post, setPosts] = useState([]);
 
-export default Feed
+  const handleSearchChange = (e) => {};
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch("api/prompt");
+      const data = await response.json();
+      setPosts(data);
+    };
+
+    fetchPost();
+  }, []);
+
+  return (
+    <section className="feed">
+      <from className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or user name"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </from>
+      <PromptCardList data={post} handleTagClick={() => {}} />
+    </section>
+  );
+};
+
+export default Feed;
