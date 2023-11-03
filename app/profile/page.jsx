016d95e426c40bbc63150cdfcbf1396827d1bd05
Alice West <alicewest.create@gmail.com>
2023-11-03 13:14:43 +0000

    wip

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -7,6 +7,8 @@ import Profile from '@components/profile'
 
 const MyProfile = () => {
 
+  const router = useRouter()
+
     const { data: session } = useSession()
     const [ myPosts, setMyPosts ] = useState([])
 
@@ -22,10 +24,27 @@ const MyProfile = () => {
         if (session?.user.id) fetchPosts()
       },[session?.user.id])
 
-    const handleEdit = () => {
+    const handleEdit = (post) => {
+      router.push(`/update-prompt?id=${post._id}`)
 
     }
-    const handleDelete = async () => {
+    const handleDelete = async (post) => {
+      const hasConfirmed = confirm("Are you sure you want to delete this prompt?")
+
+      if(hasConfirmed) {
+        try {
+          await fetch(`/api/prompt/${post._id.toString()}`, {
+            method: 'DELETE'
+          })
+
+          const filteredPosts = myPosts.filter((p) => p._id !== post._id)
+          setMyPosts(filteredPosts)
+          
+        } catch (error) {
+          console.log(error)
+          
+        }
+      }
 
     }
 
